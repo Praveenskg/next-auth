07aeee8c18da30c1fb3787c1a3a0cc03bf88e6bf
Praveen Singh <Praveensinghiics@gmail.com>
2024-02-15 20:09:28 +0530

    ⚡️ Loader

--- app/page.js
+++ app/page.js
@@ -6,7 +6,24 @@ const HomePage = () => {
   const { user } = useAuth();
 
   if (!user) {
-    return <div>loading...</div>;
+    return (
+      <div className="flex justify-center items-center h-full">
+        <div className="flex flex-row gap-2">
+          <div
+            className="w-4 h-4 rounded-full bg-blue-700 animate-bounce"
+            style={{ animationDelay: ".7s" }}
+          ></div>
+          <div
+            className="w-4 h-4 rounded-full bg-blue-700 animate-bounce"
+            style={{ animationDelay: ".3s" }}
+          ></div>
+          <div
+            className="w-4 h-4 rounded-full bg-blue-700 animate-bounce"
+            style={{ animationDelay: ".7s" }}
+          ></div>
+        </div>
+      </div>
+    );
   }
 
   return (
